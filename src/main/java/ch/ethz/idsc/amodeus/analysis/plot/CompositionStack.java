0ab4fd0284895005cd943f1486f537dbff95115b
andy2804 <andy2804@github.com>
2018-03-28 21:46:59 +0200

    Small Bugfix in the HtmlReport

--- src/main/java/ch/ethz/idsc/amodeus/analysis/plot/CompositionStack.java
+++ src/main/java/ch/ethz/idsc/amodeus/analysis/plot/CompositionStack.java
@@ -16,7 +16,7 @@ import org.jfree.ui.RectangleEdge;
 public enum CompositionStack {
     ;
 
-    private static final int WIDTH = 1000; /* Width of the image */
+    private static final int WIDTH = 700; /* Width of the image */
     private static final int HEIGHT = 125; /* Height of the image */
 
     public static void of(File directory, String fileTitle, String diagramTitle, //
