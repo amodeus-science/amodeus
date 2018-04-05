e98cb1b2cf5a26e791552fa3ba8f6df5d094188b
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-04-05 10:06:01 +0200

    formatting, linklayer

--- src/main/java/ch/ethz/idsc/amodeus/gfx/LinkLayer.java
+++ src/main/java/ch/ethz/idsc/amodeus/gfx/LinkLayer.java
@@ -4,6 +4,8 @@ package ch.ethz.idsc.amodeus.gfx;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.JCheckBox;
 
@@ -11,30 +13,56 @@ import ch.ethz.idsc.amodeus.net.OsmLink;
 import ch.ethz.idsc.amodeus.net.SimulationObject;
 import ch.ethz.idsc.amodeus.util.gui.RowPanel;
 
+class Street {
+    Point p1;
+    Point p2;
+}
+
 public class LinkLayer extends ViewerLayer {
-
+    private static final int MAX_COUNT = 16384 / 2;
+    // ---
     private static final Color LINKCOLOR = new Color(153, 153, 102, 64);
-
-    private boolean drawLinks = false;
+    private boolean drawLinks = true; // false;
+    private int count = 0;
 
     @Override
     protected void paint(Graphics2D graphics, SimulationObject ref) {
         if (drawLinks) {
             graphics.setColor(LINKCOLOR);
+            List<Street> list = new LinkedList<>();
             for (OsmLink osmLink : amodeusComponent.db.getOsmLinks()) {
                 Point p1 = amodeusComponent.getMapPosition(osmLink.getCoordFrom());
                 if (p1 != null) {
                     Point p2 = amodeusComponent.getMapPositionAlways(osmLink.getCoordTo());
-                    graphics.drawLine(p1.x, p1.y, p2.x, p2.y);
+                    Street street = new Street();
+                    street.p1 = p1;
+                    street.p2 = p2;
+                    list.add(street);
+                    if (MAX_COUNT < list.size()) {
+                        list.clear();
+                        break;
+                    }
                 }
             }
+            for (Street street : list) {
+                Point p1 = street.p1;
+                Point p2 = street.p2;
+                if (p1.equals(p2))
+                    graphics.drawOval(p1.x - 1, p1.y - 1, 3, 3);
+                else
+                    graphics.drawLine(p1.x, p1.y, p2.x, p2.y);
+            }
+            count = list.size();
         }
     }
 
     @Override
     protected void hud(Graphics2D graphics, SimulationObject ref) {
         if (drawLinks) {
-            amodeusComponent.append("%5d streets", amodeusComponent.db.getOsmLinksSize());
+            if (0 < count)
+                amodeusComponent.append("%5d/%5d streets", count, amodeusComponent.db.getOsmLinksSize());
+            else
+                amodeusComponent.append("too many streets");
             amodeusComponent.appendSeparator();
         }
     }
@@ -52,5 +80,4 @@ public class LinkLayer extends ViewerLayer {
         jCheckBox.addActionListener(event -> setDraw(jCheckBox.isSelected()));
         rowPanel.add(jCheckBox);
     }
-
 }
