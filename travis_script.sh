8cf0ebfed0dab550870d1cad2429e4eaa3758178
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-05-05 07:43:33 +0200

    travis script quieter

--- travis_script.sh
+++ travis_script.sh
@@ -4,36 +4,36 @@
 mkdir -p /home/$USER/glpk_src
 cd /home/$USER/glpk_src
 rm -rf glpk-4.65*
-wget http://ftp.gnu.org/gnu/glpk/glpk-4.65.tar.gz
+wget --quiet http://ftp.gnu.org/gnu/glpk/glpk-4.65.tar.gz
 tar -xzf glpk-4.65.tar.gz
 rm -rf glpk-java-1.12.0*
 # wget http://download.sourceforge.net/project/glpk-java/\
 # glpk-java/glpk-java-1.12.0/libglpk-java-1.12.0.tar.gz
 
-wget https://polybox.ethz.ch/index.php/s/JjWmFgYNQT4O6Bx/download
+wget --quiet https://polybox.ethz.ch/index.php/s/JjWmFgYNQT4O6Bx/download
 mv download libglpk-java-1.12.0.tar.gz
 
 tar -xzf libglpk-java-1.12.0.tar.gz
 
 # Build and install GLPK
 cd /home/$USER/glpk_src/glpk-4.65
-./configure --prefix=/home/$USER/glpk
-make -j6
-make check
-make install
+./configure --prefix=/home/$USER/glpk > /dev/null
+make --quiet -j6
+make --quiet check
+make --quiet install
 
 # Build and install GLPK for Java
 cd /home/$USER/glpk_src/libglpk-java-1.12.0
 export CPPFLAGS=-I/home/$USER/glpk/include
 export SWIGFLAGS=-I/home/$USER/glpk/include
 export LD_LIBRARY_PATH=/home/$USER/glpk/lib
-./configure --prefix=/home/$USER/glpk
+./configure --prefix=/home/$USER/glpk > /dev/null
 
 # read -n 1 -s -r -p "Press any key to continue"
 
-make
-make check
-make install
+make --quiet
+make --quiet check
+make --quiet install
 unset CPPFLAGS
 unset SWIGFLAGS
 
