f2a07f67c6b1c00ceeae5605de5fdb5ad1812155
Lukas Sieber <sieber.lukas@gmail.com>
2018-06-27 14:08:41 +0200

    Bind Shared Dispatcher

--- src/main/java/ch/ethz/idsc/amodeus/dispatcher/shared/SimpleSharedDispatcher.java
+++ src/main/java/ch/ethz/idsc/amodeus/dispatcher/shared/SimpleSharedDispatcher.java
@@ -1,13 +1,30 @@
 package ch.ethz.idsc.amodeus.dispatcher.shared;
 
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.router.util.TravelTime;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import ch.ethz.idsc.amodeus.dispatcher.FeedforwardFluidicRebalancingPolicy;
 import ch.ethz.idsc.amodeus.dispatcher.core.SharedRoboTaxi;
 import ch.ethz.idsc.amodeus.dispatcher.core.SharedUniversalDispatcher;
+import ch.ethz.idsc.amodeus.dispatcher.util.AbstractVehicleDestMatcher;
+import ch.ethz.idsc.amodeus.dispatcher.util.AbstractVirtualNodeDest;
+import ch.ethz.idsc.amodeus.dispatcher.util.EuclideanDistanceFunction;
+import ch.ethz.idsc.amodeus.dispatcher.util.HungarBiPartVehicleDestMatcher;
+import ch.ethz.idsc.amodeus.dispatcher.util.RandomVirtualNodeDest;
 import ch.ethz.idsc.amodeus.matsim.SafeConfig;
+import ch.ethz.idsc.amodeus.traveldata.TravelData;
+import ch.ethz.idsc.amodeus.virtualnetwork.VirtualNetwork;
 import ch.ethz.matsim.av.config.AVDispatcherConfig;
+import ch.ethz.matsim.av.config.AVGeneratorConfig;
+import ch.ethz.matsim.av.dispatcher.AVDispatcher;
+import ch.ethz.matsim.av.dispatcher.AVDispatcher.AVDispatcherFactory;
+import ch.ethz.matsim.av.framework.AVModule;
 import ch.ethz.matsim.av.passenger.AVRequest;
 import ch.ethz.matsim.av.plcpc.ParallelLeastCostPathCalculator;
 
@@ -46,5 +63,35 @@ public class SimpleSharedDispatcher extends SharedUniversalDispatcher {
 		}
 
 	}
+	
+	 public static class Factory implements AVDispatcherFactory {
+	        @Inject
+	        @Named(AVModule.AV_MODE)
+	        private ParallelLeastCostPathCalculator router;
+
+	        @Inject
+	        @Named(AVModule.AV_MODE)
+	        private TravelTime travelTime;
+
+	        @Inject
+	        private EventsManager eventsManager;
+
+	        @Inject(optional = true)
+	        private TravelData travelData;
+
+	        @Inject
+	        private Config config;
+
+	        @Override
+	        public AVDispatcher createDispatcher(AVDispatcherConfig avconfig) {
+	            AVGeneratorConfig generatorConfig = avconfig.getParent().getGeneratorConfig();
+
+	            AbstractVirtualNodeDest abstractVirtualNodeDest = new RandomVirtualNodeDest();
+	            AbstractVehicleDestMatcher abstractVehicleDestMatcher = new HungarBiPartVehicleDestMatcher(new EuclideanDistanceFunction());
+
+	            return new SimpleSharedDispatcher(config, avconfig, travelTime,
+	            		router, eventsManager);
+	        }
+	    }
 
 }
