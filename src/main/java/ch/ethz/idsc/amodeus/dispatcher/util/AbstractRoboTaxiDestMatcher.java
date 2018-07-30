20248343873f9f9b7ffd89167ff45ef0159426dc
clruch <claudio.ruch@gmail.com>
2018-07-30 16:13:04 +0200

    renamed class, refacto finished.

--- src/main/java/ch/ethz/idsc/amodeus/dispatcher/util/AbstractRoboTaxiDestMatcher.java
+++ src/main/java/ch/ethz/idsc/amodeus/dispatcher/util/AbstractRoboTaxiDestMatcher.java
@@ -10,22 +10,28 @@ import org.matsim.api.core.v01.network.Link;
 import ch.ethz.idsc.amodeus.dispatcher.core.RoboTaxi;
 import ch.ethz.matsim.av.passenger.AVRequest;
 
+/** abstract base class for all implementations that match {@link Collection}s of {@link RoboTaxi}s
+ * and {@link Collection} of {@link AVRequest}s or of {@link Link}s */
 public abstract class AbstractRoboTaxiDestMatcher {
 
-    protected abstract Map<RoboTaxi, AVRequest> protected_match(Collection<RoboTaxi> vehicleLinkPairs, Collection<AVRequest> links);
-
-    protected abstract Map<RoboTaxi, Link> protected_matchLink(Collection<RoboTaxi> vehicleLinkPairs, Collection<Link> links);
-
+    /** @return {@link Map} containing matching between {@link RoboTaxi}s in {@link Collection} @param roboTaxis
+     *         and {@link AVRequest}s in @param avRequests or empty colletion if either is empty */
     public final Map<RoboTaxi, AVRequest> match(Collection<RoboTaxi> roboTaxis, Collection<AVRequest> avRequests) {
         if (roboTaxis.isEmpty() || avRequests.isEmpty())
             return Collections.emptyMap();
         return protected_match(roboTaxis, avRequests);
     }
 
+    /** @return {@link Map} containing matching between {@link RoboTaxi}s in {@link Collection} @param roboTaxis
+     *         and {@link Link}s in @param links or empty colletion if either is empty */
     public final Map<RoboTaxi, Link> matchLink(Collection<RoboTaxi> roboTaxis, Collection<Link> links) {
         if (roboTaxis.isEmpty() || links.isEmpty())
             return Collections.emptyMap();
         return protected_matchLink(roboTaxis, links);
     }
+
+    protected abstract Map<RoboTaxi, AVRequest> protected_match(Collection<RoboTaxi> roboTaxis, Collection<AVRequest> avRequests);
+
+    protected abstract Map<RoboTaxi, Link> protected_matchLink(Collection<RoboTaxi> roboTaxis, Collection<Link> links);
 
 }
