b81bb58a6032e39ffe457fc4af2bf739d7c7a469
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-08-25 04:11:36 +0200

    population tools test

--- src/test/java/ch/ethz/idsc/amodeus/traveldata/PopulationToolsTest.java
+++ src/test/java/ch/ethz/idsc/amodeus/traveldata/PopulationToolsTest.java
@@ -2,10 +2,10 @@
 package ch.ethz.idsc.amodeus.traveldata;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -30,15 +30,17 @@ import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.alg.Array;
 
+// TODO the previous implementation used links from VN3 to also create requests for VN2
+// ... create a separate file for tests regarding VN2 so that the links don't get mixed
 public class PopulationToolsTest {
-    private static VirtualNetwork<Link> virtualNetwork2;
+    // private static VirtualNetwork<Link> virtualNetwork2;
     private static VirtualNetwork<Link> virtualNetwork3;
     private static ScenarioOptions scenarioOptions;
     private static Population population;
     private static Network network;
-    private static Set<Request> requestsSingle = new HashSet<>();
-    private static Set<Request> requestsEmpty = new HashSet<>();
-    private static Set<Request> requests = new HashSet<>();
+    private static Set<Request> requestsSingle3 = new HashSet<>();
+    private static Set<Request> requestsEmpty = Collections.emptySet();
+    private static Set<Request> requests3 = new HashSet<>();
 
     @BeforeClass
     public static void setup() throws IOException {
@@ -53,69 +55,70 @@ public class PopulationToolsTest {
         population = scenario.getPopulation();
 
         // create 2 node virtual network
-        scenarioOptions.setProperty(ScenarioOptionsBase.NUMVNODESIDENTIFIER, "2");
-        VirtualNetworkCreator virtualNetworkCreator = scenarioOptions.getVirtualNetworkCreator();
-        virtualNetwork2 = virtualNetworkCreator.create(network, population);
+        // scenarioOptions.setProperty(ScenarioOptionsBase.NUMVNODESIDENTIFIER, "2");
+        // VirtualNetworkCreator virtualNetworkCreator = scenarioOptions.getVirtualNetworkCreator();
+        // virtualNetwork2 = virtualNetworkCreator.create(network, population);
 
         // create 3 node virtual network
         scenarioOptions.setProperty(ScenarioOptionsBase.NUMVNODESIDENTIFIER, "3");
-        virtualNetworkCreator = scenarioOptions.getVirtualNetworkCreator();
+        VirtualNetworkCreator virtualNetworkCreator = scenarioOptions.getVirtualNetworkCreator();
         virtualNetwork3 = virtualNetworkCreator.create(network, population);
 
         Link node0 = (Link) virtualNetwork3.getVirtualNode(0).getLinks().toArray()[0]; // in both virtual networks in virtual node 0
         Link node1 = (Link) virtualNetwork3.getVirtualNode(1).getLinks().toArray()[0]; // in virtualNetwork2 in virtual node 0
         Link node2 = (Link) virtualNetwork3.getVirtualNode(2).getLinks().toArray()[0]; // in virtualNetwork2 in virtual node 1
 
-        requestsSingle.add(new Request(10, node0, node2));
-        requests.add(new Request(0, node0, node2));
-        requests.add(new Request(3600, node1, node2));
-        requests.add(new Request(24 * 3600 - 1, node1, node0));
-        requests.add(new Request(3600, node2, node2));
+        requestsSingle3.add(new Request(10, node0, node2));
+        requests3.add(new Request(0, node0, node2));
+        requests3.add(new Request(3600, node1, node2));
+        requests3.add(new Request(24 * 3600 - 1, node1, node0));
+        requests3.add(new Request(3600, node2, node2));
 
     }
 
-    @Test
-    public void testInvalid() {
-        try {
-            PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsSingle, virtualNetwork2, 3601);
-            assertTrue(false);
-        } catch (Exception exception) {
-            // ---
-        }
-
-        try {
-            PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsSingle, virtualNetwork2, -1);
-            assertTrue(false);
-        } catch (Exception exception) {
-            // ---
-        }
-    }
+    // @Test
+    // public void testInvalid() {
+    // try {
+    // PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsSingle3, virtualNetwork2, 3601);
+    // assertTrue(false);
+    // } catch (Exception exception) {
+    // // ---
+    // }
+    //
+    // try {
+    // PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsSingle3, virtualNetwork2, -1);
+    // assertTrue(false);
+    // } catch (Exception exception) {
+    // // ---
+    // }
+    // }
 
     @Test
     public void testEmpty() {
-        Tensor lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsEmpty, virtualNetwork2, 3600);
-        assertEquals(lambda, Array.zeros(24, 2, 2));
+        Tensor lambda;
+        // lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsEmpty, virtualNetwork2, 3600);
+        // assertEquals(lambda, Array.zeros(24, 2, 2));
 
         lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsEmpty, virtualNetwork3, 900);
         assertEquals(lambda, Array.zeros(96, 3, 3));
     }
 
-    @Test
-    public void testVirtualNetwork2() {
-        Tensor lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsSingle, virtualNetwork2, 12 * 3600);
-        assertEquals(lambda, Tensors.of(Tensors.of(Tensors.vector(0, 1), Tensors.vector(0, 0)), Tensors.of(Tensors.vector(0, 0), Tensors.vector(0, 0))));
-
-        lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requests, virtualNetwork2, 12 * 3600);
-        assertEquals(lambda, Tensors.of(Tensors.of(Tensors.vector(0, 2), Tensors.vector(0, 1)), Tensors.of(Tensors.vector(1, 0), Tensors.vector(0, 0))));
-    }
+    // @Test
+    // public void testVirtualNetwork2() {
+    // Tensor lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsSingle3, virtualNetwork2, 12 * 3600);
+    // assertEquals(lambda, Tensors.of(Tensors.of(Tensors.vector(0, 1), Tensors.vector(0, 0)), Tensors.of(Tensors.vector(0, 0), Tensors.vector(0, 0))));
+    //
+    // lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requests3, virtualNetwork2, 12 * 3600);
+    // assertEquals(lambda, Tensors.of(Tensors.of(Tensors.vector(0, 2), Tensors.vector(0, 1)), Tensors.of(Tensors.vector(1, 0), Tensors.vector(0, 0))));
+    // }
 
     @Test
     public void testVirtualNetwork3() {
-        Tensor lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsSingle, virtualNetwork3, 12 * 3600);
+        Tensor lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requestsSingle3, virtualNetwork3, 12 * 3600);
         assertEquals(lambda, Tensors.of(Tensors.of(Tensors.vector(0, 0, 1), Tensors.vector(0, 0, 0), Tensors.vector(0, 0, 0)),
                 Tensors.of(Tensors.vector(0, 0, 0), Tensors.vector(0, 0, 0), Tensors.vector(0, 0, 0))));
 
-        lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requests, virtualNetwork3, 12 * 3600);
+        lambda = PopulationTools.getLambdaInVirtualNodesAndTimeIntervals(requests3, virtualNetwork3, 12 * 3600);
         assertEquals(lambda, Tensors.of(Tensors.of(Tensors.vector(0, 0, 1), Tensors.vector(0, 0, 1), Tensors.vector(0, 0, 1)),
                 Tensors.of(Tensors.vector(0, 0, 0), Tensors.vector(1, 0, 0), Tensors.vector(0, 0, 0))));
     }
