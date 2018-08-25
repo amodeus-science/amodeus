a54a03085be27e573e033b91cf5f8a0ef5d06d7c
fluric <fluric@student.ethz.ch>
2018-08-25 10:26:32 +0200

    applying corrections from @datahaki

--- src/main/java/ch/ethz/idsc/amodeus/traveldata/TravelDatas.java
+++
@@ -1,9 +0,0 @@
-/* amodeus - Copyright (c) 2018, ETH Zurich, Institute for Dynamic Systems and Control */
-package ch.ethz.idsc.amodeus.traveldata;
-
-public enum TravelDatas {
-
-    ;
-    public static final int DURATION = 24 * 60 * 60;
-
-}
