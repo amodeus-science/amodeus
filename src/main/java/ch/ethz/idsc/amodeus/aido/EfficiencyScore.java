bbb45e070cc9a8e5ebc5a7fa9eb5f0d87c5d30b7
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-09-02 10:29:14 +0200

    scenarios with different linkSpeedData filename

--- src/main/java/ch/ethz/idsc/amodeus/aido/EfficiencyScore.java
+++ src/main/java/ch/ethz/idsc/amodeus/aido/EfficiencyScore.java
@@ -3,17 +3,15 @@ package ch.ethz.idsc.amodeus.aido;
 
 import java.util.Properties;
 
-import ch.ethz.idsc.amodeus.util.math.SI;
-import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
-import ch.ethz.idsc.tensor.qty.Quantity;
 
 /* package */ class EfficiencyScore extends LinComScore {
-    protected static Tensor setAlpha(Properties scrprm) {
-        Scalar alpha3 = Quantity.of(Double.parseDouble(scrprm.getProperty("alpha3")), SI.SECOND.negate());
-        Scalar alpha4 = Quantity.of(Double.parseDouble(scrprm.getProperty("alpha4")), SI.METER.negate());
-        return Tensors.of(alpha3, alpha4);
+    protected static Tensor setAlpha(Properties properties) {
+        return Tensors.of( //
+                Scalars.fromString(properties.getProperty("alpha3")), //
+                Scalars.fromString(properties.getProperty("alpha4")));
     }
 
     /* package */ EfficiencyScore(Properties properties) {
--- src/main/java/ch/ethz/idsc/amodeus/aido/ServiceQualityScore.java
+++ src/main/java/ch/ethz/idsc/amodeus/aido/ServiceQualityScore.java
@@ -1,26 +1,21 @@
 /* amodeus - Copyright (c) 2018, ETH Zurich, Institute for Dynamic Systems and Control */
 package ch.ethz.idsc.amodeus.aido;
 
-import java.util.Objects;
 import java.util.Properties;
 
-import ch.ethz.idsc.amodeus.util.math.SI;
-import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
-import ch.ethz.idsc.tensor.qty.Quantity;
 
 /* package */ class ServiceQualityScore extends LinComScore {
-    protected static Tensor setAlpha(Properties scrprm) {
-        Scalar alpha1 = Quantity.of(Double.parseDouble(scrprm.getProperty("alpha1")), SI.SECOND.negate());
-        Objects.requireNonNull(alpha1);
-        Scalar alpha2 = Quantity.of(Double.parseDouble(scrprm.getProperty("alpha2")), SI.METER.negate());
-        Objects.requireNonNull(alpha2);
-        return Tensors.of(alpha1, alpha2);
+    protected static Tensor setAlpha(Properties properties) {
+        return Tensors.of( //
+                Scalars.fromString(properties.getProperty("alpha1")), //
+                Scalars.fromString(properties.getProperty("alpha2")));
     }
 
-    public ServiceQualityScore(Properties scrprm) {
-        super(setAlpha(scrprm));
+    /* package */ ServiceQualityScore(Properties properties) {
+        super(setAlpha(properties));
     }
 
 }
