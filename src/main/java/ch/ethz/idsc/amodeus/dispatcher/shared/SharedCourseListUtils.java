a9251dffbce7fb0eaed7a6ceb58348d4b2efa3a4
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-11-13 14:14:29 +0100

    file header

--- src/main/java/ch/ethz/idsc/amodeus/dispatcher/shared/SharedCourseListUtils.java
+++ src/main/java/ch/ethz/idsc/amodeus/dispatcher/shared/SharedCourseListUtils.java
@@ -21,7 +21,7 @@ public enum SharedCourseListUtils {
     }
 
     // **************************************************
-    // Get Funcitons
+    // Get Functions
     // **************************************************
 
     public static Set<AVRequest> getOnBoardRequests(List<? extends SharedCourse> courses) {
