a0bdbe093e1cafbda304230be1ba5f7454677c2d
clruch <claudio.ruch@gmail.com>
2018-11-23 07:22:22 +0100

    added generator to run FeedforwardFluidicTimeVarying policy.

--- src/main/java/ch/ethz/idsc/amodeus/matsim/mod/AmodeusVehicleGeneratorModule.java
+++ src/main/java/ch/ethz/idsc/amodeus/matsim/mod/AmodeusVehicleGeneratorModule.java
@@ -8,8 +8,15 @@ import ch.ethz.matsim.av.framework.AVUtils;
 public class AmodeusVehicleGeneratorModule extends AbstractModule {
     @Override
     public void install() {
+
+        /** places vehicles at random sampling according to request density */
         bind(RandomDensityGenerator.Factory.class);
         AVUtils.bindGeneratorFactory(binder(), RandomDensityGenerator.class.getSimpleName()).//
                 to(RandomDensityGenerator.Factory.class);
+
+        /** generator used for {@link FeedforwardFluidicTimeVaryingRebalancingPolicy} */
+        bind(VehicleToVSGenerator.Factory.class);
+        AVUtils.bindGeneratorFactory(binder(), VehicleToVSGenerator.class.getSimpleName()).//
+                to(VehicleToVSGenerator.Factory.class);
     }
 }
