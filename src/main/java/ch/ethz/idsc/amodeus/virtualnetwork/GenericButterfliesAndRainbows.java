daae8546ed2fa20d318caef78deb7c836fa5c708
clruch <claudio.ruch@gmail.com>
2019-01-07 09:27:00 +0100

    made file public for usage in consensus.

--- src/main/java/ch/ethz/idsc/amodeus/virtualnetwork/GenericButterfliesAndRainbows.java
+++ src/main/java/ch/ethz/idsc/amodeus/virtualnetwork/GenericButterfliesAndRainbows.java
@@ -14,7 +14,7 @@ import java.util.Set;
 /** class helps to detect adjacency between VirtualNodes based on a topological criterion. For every
  * VirtualNode a list of elements is supplied, if an element is contained in two virtualNodes, they
  * share a virtual Link. */
-/* package */ class GenericButterfliesAndRainbows<T, U> {
+public class GenericButterfliesAndRainbows<T, U> {
     private final Map<U, Set<VirtualNode<T>>> map = new HashMap<>();
 
     public void add(U node, VirtualNode<T> virtualNode) {
