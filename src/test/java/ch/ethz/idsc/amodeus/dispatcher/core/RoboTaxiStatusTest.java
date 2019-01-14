d5d11a3fef92fef35343347f4144005f119ee23f
Lukas Sieber <sieber.lukas@gmail.com>
2019-01-14 15:49:34 +0100

    remove test

--- src/test/java/ch/ethz/idsc/amodeus/dispatcher/core/RoboTaxiStatusTest.java
+++ src/test/java/ch/ethz/idsc/amodeus/dispatcher/core/RoboTaxiStatusTest.java
@@ -14,33 +14,4 @@ public class RoboTaxiStatusTest extends TestCase {
         assertFalse(RoboTaxiStatus.OFFSERVICE.isDriving());
     }
 
-    public void testGetIsDroppingOf() {
-
-//        ArtificialScenarioCreator s = new ArtificialScenarioCreator();
-//        s.roboTaxi1.addAVRequestToMenu(s.avRequest1);
-//        s.roboTaxi1.setDivertableLinkTime(new LinkTimePair(s.linkUp, 1.0));
-//        s.roboTaxi1.pickupNewCustomerOnBoard();
-//        s.roboTaxi1.setDivertableLinkTime(new LinkTimePair(s.linkDown, 20.0));
-//
-//        assertFalse(s.roboTaxi1.getDropoffInProgress());
-//
-//        s.roboTaxi1.startDropoff();
-//        // Assign Directive To roboTaxi
-//        // TravelTime travelTime = new FreeSpeedTravelTime();
-//        // Future<Path> futurePath;
-//        // FuturePathContainer futurePathContainer = new FuturePathContainer(s.avRequest1.getFromLink(), s.avRequest1.getToLink(), 20.0, futurePath, travelTime);
-//        // s.roboTaxi1.assignDirective(new SharedGeneralDropoffDirective(s.roboTaxi1, s.avRequest1, futurePathContainer, 20.0, 10.0));
-//
-//        assertTrue(s.roboTaxi1.getDropoffInProgress());
-//        // assertFalse(s.roboTaxi1.isDivertable());
-//
-//        // s.roboTaxi1.executeDirective();
-//        // assertTrue(s.roboTaxi1.getDropoffInProgress());
-//        // assertFalse(s.roboTaxi1.isDivertable());
-//
-//        s.roboTaxi1.dropOffCustomer();
-//
-//        assertFalse(s.roboTaxi1.getDropoffInProgress());
-
-    }
 }
