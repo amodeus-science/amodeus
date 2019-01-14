857d5d6b15c32be3afddf19d0a6667620967057c
Lukas Sieber <sieber.lukas@gmail.com>
2019-01-14 10:00:16 +0100

    Test for get Dropoff Function in RoboTaxi

--- src/test/java/ch/ethz/idsc/amodeus/dispatcher/core/RoboTaxiStatusTest.java
+++ src/test/java/ch/ethz/idsc/amodeus/dispatcher/core/RoboTaxiStatusTest.java
@@ -1,6 +1,12 @@
 /* amodeus - Copyright (c) 2018, ETH Zurich, Institute for Dynamic Systems and Control */
 package ch.ethz.idsc.amodeus.dispatcher.core;
 
+import java.util.Optional;
+
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.dvrp.util.LinkTimePair;
+
+import ch.ethz.idsc.amodeus.dispatcher.shared.SharedCourse;
 import junit.framework.TestCase;
 
 public class RoboTaxiStatusTest extends TestCase {
@@ -10,5 +16,35 @@ public class RoboTaxiStatusTest extends TestCase {
         assertTrue(RoboTaxiStatus.DRIVEWITHCUSTOMER.isDriving());
         assertFalse(RoboTaxiStatus.STAY.isDriving());
         assertFalse(RoboTaxiStatus.OFFSERVICE.isDriving());
+    }
+    
+    public void testGetIsDroppingOf() {
+        
+        ArtificialScenarioCreator s = new ArtificialScenarioCreator();
+        
+        s.roboTaxi1.addAVRequestToMenu(s.avRequest1);
+        s.roboTaxi1.setDivertableLinkTime(new LinkTimePair(s.linkUp, 1.0));
+        s.roboTaxi1.pickupNewCustomerOnBoard();
+        s.roboTaxi1.setDivertableLinkTime(new LinkTimePair(s.linkDown, 20.0));
+        
+        assertFalse(s.roboTaxi1.getDropoffInProgress());
+        
+        s.roboTaxi1.startDropoff();
+        // Assign Directive To roboTaxi
+        final double endDropOffTime = 30.0;
+        Optional<SharedCourse> secondCourse = RoboTaxiUtils.getSecondCourse(s.roboTaxi1);
+        final Link endLink = (secondCourse.isPresent()) ? secondCourse.get().getLink() : s.avRequest1.getToLink();
+//        FuturePathContainer futurePathContainer = futurePathFactory.createFuturePathContainer(s.avRequest1.getToLink(), endLink, endDropOffTime);
+        FuturePathContainer futurePathContainer = null;
+        s.roboTaxi1.assignDirective(new SharedGeneralDropoffDirective(s.roboTaxi1, s.avRequest1, futurePathContainer, 20.0, 10.0));
+
+        
+        assertTrue(s.roboTaxi1.getDropoffInProgress());
+        assertFalse(s.roboTaxi1.isDivertable());
+        
+        s.roboTaxi1.dropOffCustomer();
+        
+        assertFalse(s.roboTaxi1.getDropoffInProgress());
+        
     }
 }
