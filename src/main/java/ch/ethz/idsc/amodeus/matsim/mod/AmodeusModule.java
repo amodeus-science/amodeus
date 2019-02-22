48407f36694c501bfb35fd0f84d4b0f9218e0d6c
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-02-22 18:46:52 +0100

    Update to latest stable MATSim + av

--- src/main/java/ch/ethz/idsc/amodeus/matsim/mod/AmodeusModule.java
+++ src/main/java/ch/ethz/idsc/amodeus/matsim/mod/AmodeusModule.java
@@ -4,10 +4,12 @@ package ch.ethz.idsc.amodeus.matsim.mod;
 import java.util.Collection;
 
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.contrib.dvrp.run.DvrpModule;
+import org.matsim.contrib.dvrp.router.DvrpRoutingNetworkProvider;
+import org.matsim.contrib.dvrp.trafficmonitoring.DvrpTravelTimeModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.mobsim.qsim.AbstractQSimPlugin;
+import org.matsim.core.trafficmonitoring.FreeSpeedTravelTime;
 
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
@@ -19,7 +21,11 @@ import ch.ethz.matsim.av.framework.AVQSimPlugin;
 public class AmodeusModule extends AbstractModule {
     @Override
     public void install() {
-        // ---
+        /* This has been added after upgrading Amodeus' dependency to MATSim:0.10.1. In the mean time, DVRP got a new "initial travel time" which resembles better what
+         * QSim produces (since the time-step (seconds) based QSim produces travel times that are "rounded" to the next integer). This is now considered in DVRP 0.10.1,
+         * but it was not when Amodeus' unit tests had been written. Hence, to leave the unit tests intact, we add here the old FreeSpeedTravelTime. For reference, the
+         * MATSim commit, that introduced the new TravelTime implementation in DVRP is be380cfc72e1c29d840fcc6b71a4bebeb3e567c1 . */
+        addTravelTimeBinding(DvrpTravelTimeModule.DVRP_INITIAL).toInstance(new FreeSpeedTravelTime());
     }
 
     @Provides
@@ -36,7 +42,7 @@ public class AmodeusModule extends AbstractModule {
 
     @Provides
     @Singleton
-    @Named(DvrpModule.DVRP_ROUTING)
+    @Named(DvrpRoutingNetworkProvider.DVRP_ROUTING)
     public Network provideAVNetwork(Network fullNetwork) {
         /* TODO MISC Here we provide the FULL network with public transit links etc.,
          * because this is how Amodeus has been set up initially. This was not a problem,
