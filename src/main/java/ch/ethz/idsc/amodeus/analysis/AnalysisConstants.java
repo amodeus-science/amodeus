aeaa1c9ce80634f4545e4112ce74958dd24f9bb8
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-05-02 21:20:29 +0200

    v173

--- src/main/java/ch/ethz/idsc/amodeus/analysis/AnalysisConstants.java
+++ src/main/java/ch/ethz/idsc/amodeus/analysis/AnalysisConstants.java
@@ -1,7 +1,9 @@
+/* amodeus - Copyright (c) 2019, ETH Zurich, Institute for Dynamic Systems and Control */
 package ch.ethz.idsc.amodeus.analysis;
 
-public class AnalysisConstants {
+public enum AnalysisConstants {
+    ;
 
     public static final String ParametersExportFilename = "scenarioParameters.obj";
-    
+
 }
