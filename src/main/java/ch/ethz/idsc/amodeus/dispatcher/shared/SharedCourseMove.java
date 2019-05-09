43f734aef597fd700d9972a23ada8792211791c0
clruch <claudio.ruch@gmail.com>
2019-05-09 12:41:57 +0200

    further cleaning up.

--- src/main/java/ch/ethz/idsc/amodeus/dispatcher/shared/SharedCourseMove.java
+++ src/main/java/ch/ethz/idsc/amodeus/dispatcher/shared/SharedCourseMove.java
@@ -8,38 +8,6 @@ import ch.ethz.idsc.amodeus.util.math.GlobalAssert;
 
 public enum SharedCourseMove {
     ;
-    
-    // **************************************************
-    // MOVING COURSES
-    // **************************************************
-
-    public static boolean moveAVCourseToPrev(List<SharedCourse> courses, SharedCourse sharedAVCourse) {
-        GlobalAssert.that(courses.contains(sharedAVCourse));
-        int i = courses.indexOf(sharedAVCourse);
-        if (0 < i && i < courses.size()) {
-            Collections.swap(courses, i, i - 1);
-            return true;
-        }
-        System.out.println("Swaping Failed!!");
-        return false;
-    }
-
-    public static boolean moveAVCourseToNext(List<SharedCourse> courses, SharedCourse sharedAVCourse) {
-        GlobalAssert.that(courses.contains(sharedAVCourse));
-        int i = courses.indexOf(sharedAVCourse);
-        if (0 <= i && i < courses.size() - 1) {
-            Collections.swap(courses, i, i + 1);
-            return true;
-        }
-        System.out.println("Swaping Failed!!");
-        return false;
-    }
-    
-    
-    
-    // **************************************************
-    // MOVING COURSES
-    // **************************************************
 
     public static SharedMenu moveAVCourseToPrev(SharedMenu sharedMenu, SharedCourse sharedAVCourse) {
         return applyFunction(sharedMenu, SharedCourseMove::moveAVCourseToPrev, sharedAVCourse);
@@ -49,9 +17,10 @@ public enum SharedCourseMove {
         return applyFunction(sharedMenu, SharedCourseMove::moveAVCourseToNext, sharedAVCourse);
     }
 
-    
-    
-    private static SharedMenu applyFunction(SharedMenu sharedMenu, BiConsumer<List<SharedCourse>, SharedCourse> listFunction, SharedCourse... sharedCourses) {
+    // --
+
+    private static SharedMenu applyFunction(SharedMenu sharedMenu, BiConsumer<List<SharedCourse>, SharedCourse> listFunction, //
+            SharedCourse... sharedCourses) {
         List<SharedCourse> list = SharedCourseListUtils.copy(sharedMenu.getRoboTaxiMenu());
         for (SharedCourse sharedCourse : sharedCourses) {
             listFunction.accept(list, sharedCourse);
@@ -59,5 +28,26 @@ public enum SharedCourseMove {
         return SharedMenu.of(list);
     }
 
+    private static boolean moveAVCourseToPrev(List<SharedCourse> courses, SharedCourse sharedAVCourse) {
+        GlobalAssert.that(courses.contains(sharedAVCourse));
+        int i = courses.indexOf(sharedAVCourse);
+        if (0 < i && i < courses.size()) {
+            Collections.swap(courses, i, i - 1);
+            return true;
+        }
+        System.out.println("Swapping Failed!!");
+        return false;
+    }
+
+    private static boolean moveAVCourseToNext(List<SharedCourse> courses, SharedCourse sharedAVCourse) {
+        GlobalAssert.that(courses.contains(sharedAVCourse));
+        int i = courses.indexOf(sharedAVCourse);
+        if (0 <= i && i < courses.size() - 1) {
+            Collections.swap(courses, i, i + 1);
+            return true;
+        }
+        System.out.println("Swapping Failed!!");
+        return false;
+    }
 
 }
