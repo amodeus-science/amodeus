02684ac25603e765614ed5ad7ddc2e034985dc00
clruch <claudio.ruch@gmail.com>
2019-05-14 14:54:14 +0200

    to enable work for alice.

--- src/main/java/ch/ethz/idsc/amodeus/dispatcher/util/GlobalBipartiteWeight.java
+++ src/main/java/ch/ethz/idsc/amodeus/dispatcher/util/GlobalBipartiteWeight.java
@@ -5,7 +5,7 @@ import org.matsim.api.core.v01.network.Link;
 
 import ch.ethz.idsc.amodeus.dispatcher.core.RoboTaxi;
 
-/* package */ interface GlobalBipartiteWeight {
+public interface GlobalBipartiteWeight {
 
     /** @return the scalar cost between every pair of a {@link RoboTaxi} @param roboTaxi
      *         and a {@link Link} @param link, e.g., the Euclidean distance */
