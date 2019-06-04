c4904f6e6b4b71e25e981e6bb67cc7db428e8226
luchengqi7 <43133404+luchengqi7@users.noreply.github.com>
2019-06-04 16:02:57 +0200

    Update UniversalDispatcher.java
    add get idling (staying) roboTaxi function

--- src/main/java/ch/ethz/idsc/amodeus/dispatcher/core/UniversalDispatcher.java
+++ src/main/java/ch/ethz/idsc/amodeus/dispatcher/core/UniversalDispatcher.java
@@ -129,6 +129,12 @@ public abstract class UniversalDispatcher extends RoboTaxiMaintainer {
                 .collect(Collectors.toList());
     }
 
+    protected final Collection<RoboTaxi> getStayingTaxi() {
+        return getDivertableUnassignedRoboTaxis().stream() //
+                .filter(RoboTaxi::isInStayTask)//
+                .collect(Collectors.toList());
+    }
+
     /** @return immutable and inverted copy of pickupRegister, displays which
      *         vehicles are currently scheduled to pickup which request */
     protected final Map<RoboTaxi, AVRequest> getPickupRoboTaxis() {
