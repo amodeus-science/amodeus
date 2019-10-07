225bc24ab8b3b428fe4d8bb0f98942b1dd2d3419
amodeus <claudio.ruch@gmail.com>
2019-10-07 07:47:48 +0200

    wip

--- src/main/java/ch/ethz/idsc/amodeus/linkspeed/LSDataTravelTime.java
+++ src/main/java/ch/ethz/idsc/amodeus/linkspeed/LSDataTravelTime.java
@@ -9,29 +9,27 @@ import org.matsim.core.router.util.TravelTime;
 import org.matsim.vehicles.Vehicle;
 
 import ch.ethz.idsc.amodeus.net.MatsimAmodeusDatabase;
-import ch.ethz.idsc.tensor.Scalar;
-import ch.ethz.idsc.tensor.red.Mean;
 
 public class LSDataTravelTime implements TravelTime {
 
-    private final LinkSpeedDataContainer lsData;
-    private final MatsimAmodeusDatabase db;
+	private final LinkSpeedDataContainer lsData;
+	private final MatsimAmodeusDatabase db;
 
-    public LSDataTravelTime(LinkSpeedDataContainer lsData, MatsimAmodeusDatabase db) {
-        this.lsData = lsData;
-        this.db = db;
-    }
+	public LSDataTravelTime(LinkSpeedDataContainer lsData, MatsimAmodeusDatabase db) {
+		this.lsData = lsData;
+		this.db = db;
+	}
 
-    @Override
-    public double getLinkTravelTime(Link link, double time, Person person, Vehicle vehicle) {
-        Integer linkID = LinkIndex.fromLink(db, link);
-        double speed = link.getFreespeed();
-        LinkSpeedTimeSeries timeSeries = lsData.getLinkSet().get(linkID);
-        if (Objects.nonNull(timeSeries)) {
-            LinkSpeedTimeSeries series = lsData.getLinkSet().get(linkID);
-            speed = series.getSpeedsFloor((int) time);
-        }
-        return link.getLength() / speed;
-    }
+	@Override
+	public double getLinkTravelTime(Link link, double time, Person person, Vehicle vehicle) {
+		Integer linkID = LinkIndex.fromLink(db, link);
+		double speed = link.getFreespeed();
+		LinkSpeedTimeSeries timeSeries = lsData.getLinkSet().get(linkID);
+		if (Objects.nonNull(timeSeries)) {
+			LinkSpeedTimeSeries series = lsData.getLinkSet().get(linkID);
+			speed = series.getSpeedsFloor((int) time);
+		}
+		return link.getLength() / speed;
+	}
 
 }
