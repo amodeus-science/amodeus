92702d6c42bd3c60e2ccdbe7eccc0a6abc7590e1
clruch <claudio.ruch@gmail.com>
2019-10-15 11:28:10 +0200

    fixed test that modified resources... hello Sebastian? :D

--- src/test/java/ch/ethz/idsc/amodeus/matsim/xml/XmlAVSettingsTest.java
+++ src/test/java/ch/ethz/idsc/amodeus/matsim/xml/XmlAVSettingsTest.java
@@ -6,30 +6,42 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import ch.ethz.idsc.amodeus.util.io.CopyFiles;
 import ch.ethz.idsc.amodeus.util.io.Locate;
-import ch.ethz.idsc.amodeus.util.io.MultiFileTools;
 import ch.ethz.idsc.amodeus.util.math.GlobalAssert;
 import ch.ethz.idsc.tensor.io.DeleteDirectory;
 
 public class XmlAVSettingsTest {
 
+    private static File workingDirectory;
+
     @BeforeClass
     public static void prepare() throws Exception {
 
         /** copy av.xml file from test directory */
-        File workingDirectory = MultiFileTools.getDefaultWorkingDirectory();
         File scenarioDirectory = new File(Locate.repoFolder(XmlAVSettingsTest.class, "amodeus"), "resources/testScenario");
+
+        workingDirectory = new File(scenarioDirectory, "Temp");
+        if (!workingDirectory.isDirectory())
+            workingDirectory.mkdir();
+
         GlobalAssert.that(workingDirectory.isDirectory());
+
         CopyFiles.now(scenarioDirectory.getAbsolutePath(), workingDirectory.getAbsolutePath(), //
                 Arrays.asList(new String[] { "config_full.xml" }), true);
-        
-        
-        String configFilePath = new File(scenarioDirectory,"config_full.xml").getAbsolutePath();
+
+        System.out.println("wordir: " + workingDirectory.getAbsolutePath());
+        System.out.println("scenarioDirectory: " + scenarioDirectory.getAbsolutePath());
+
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        String configFilePath = new File(workingDirectory, "config_full.xml").getAbsolutePath();
 
         /** perform some action on it */
         ConfigVehiclesChanger.change(configFilePath, 111);
@@ -43,14 +55,9 @@ public class XmlAVSettingsTest {
 
     }
 
-    @Test
-    public void test() {
-        Assert.assertTrue(true);
-    }
-
     @AfterClass
     public static void swipeFloor() throws IOException {
-        DeleteDirectory.of(new File("config_full.xml"), 1, 1);
+        DeleteDirectory.of(workingDirectory, 1, 2);
     }
 
 }
