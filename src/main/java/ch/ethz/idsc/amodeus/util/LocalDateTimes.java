94c0b3eb984b88f581603ee757ab1ca25cbb94e0
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-10-17 19:30:14 +0200

    local date times

--- src/main/java/ch/ethz/idsc/amodeus/util/LocalDateTimes.java
+++ src/main/java/ch/ethz/idsc/amodeus/util/LocalDateTimes.java
@@ -4,10 +4,8 @@ package ch.ethz.idsc.amodeus.util;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-import ch.ethz.idsc.amodeus.util.math.GlobalAssert;
-import ch.ethz.idsc.amodeus.util.math.SI;
+import ch.ethz.idsc.amodeus.util.math.Magnitude;
 import ch.ethz.idsc.tensor.Scalar;
-import ch.ethz.idsc.tensor.qty.Quantity;
 
 public enum LocalDateTimes {
     ;
@@ -15,6 +13,7 @@ public enum LocalDateTimes {
     /** @return true if the {@link LocalDateTime} @param ldt1 is less than or equal to
      *         the {@link LocalDateTime} @param ldt2 and false otherwise. */
     public static boolean lessEquals(LocalDateTime ldt1, LocalDateTime ldt2) {
+        // TODO use ldt1.compareTo(ldt2) == XY?
         return lessThan(ldt1, ldt2) //
                 || ldt1.toInstant(ZoneOffset.UTC).toEpochMilli() == ldt2.toInstant(ZoneOffset.UTC).toEpochMilli();
     }
@@ -22,21 +21,22 @@ public enum LocalDateTimes {
     /** @return true if the {@link LocalDateTime} @param ldt1 is strictly less than
      *         the {@link LocalDateTime} @param ldt2 and false otherwise. */
     public static boolean lessThan(LocalDateTime ldt1, LocalDateTime ldt2) {
+        // TODO use ldt1.compareTo(ldt2) == XY?
         return ldt1.toInstant(ZoneOffset.UTC).toEpochMilli() < //
                 ldt2.toInstant(ZoneOffset.UTC).toEpochMilli();
     }
 
-    /** @return the {@link LocalDateTime} @param ldt to which @param seconds were added, works
-     *         also for negative amounts of seconds */
-    public static LocalDateTime addTo(LocalDateTime ldt, Scalar seconds) {
-        GlobalAssert.that(((Quantity) seconds).unit().equals(SI.SECOND));
-        long epochSecond = ldt.toInstant(ZoneOffset.UTC).toEpochMilli() / 1000 + seconds.number().intValue();
-        int nanoRest = (int) (ldt.toInstant(ZoneOffset.UTC).toEpochMilli() % 1000 * 1000000);
+    /** @return the {@link LocalDateTime} @param localDateTime to which @param duration were added, works
+     *         also for negative duration */
+    public static LocalDateTime addTo(LocalDateTime localDateTime, Scalar duration) {
+        Scalar seconds = Magnitude.SECOND.apply(duration);
+        long epochSecond = localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli() / 1000 + seconds.number().longValue();
+        int nanoRest = (int) (localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli() % 1000 * 1000000);
         return LocalDateTime.ofEpochSecond(epochSecond, nanoRest, ZoneOffset.UTC);
     }
 
-    /** @return the {@link LocalDateTime} @param ldt to which @param seconds were subtracted. */
-    public static LocalDateTime subtractFrom(LocalDateTime ldt, Scalar seconds) {
-        return addTo(ldt, seconds);
+    /** @return the {@link LocalDateTime} @param ldt to which @param duration were subtracted. */
+    public static LocalDateTime subtractFrom(LocalDateTime localDateTime, Scalar duration) {
+        return addTo(localDateTime, duration.negate());
     }
 }
