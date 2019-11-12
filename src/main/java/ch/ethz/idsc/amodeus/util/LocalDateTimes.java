e95850c8c60ece1b068b6aca9ee5cd27f502a49c
JoelGaechter <joelgaechter@bluewin.ch>
2019-11-12 15:39:05 +0100

    util edit pass part 2

--- src/main/java/ch/ethz/idsc/amodeus/util/LocalDateTimes.java
+++ src/main/java/ch/ethz/idsc/amodeus/util/LocalDateTimes.java
@@ -12,18 +12,20 @@ public enum LocalDateTimes {
 
     /** @return true if the {@link LocalDateTime} @param ldt1 is less than or equal to
      *         the {@link LocalDateTime} @param ldt2 and false otherwise. */
+    @Deprecated // (almost) redundant, see below
     public static boolean lessEquals(LocalDateTime ldt1, LocalDateTime ldt2) {
-        // TODO use ldt1.compareTo(ldt2) == XY?
-        return lessThan(ldt1, ldt2) //
-                || ldt1.toInstant(ZoneOffset.UTC).toEpochMilli() == ldt2.toInstant(ZoneOffset.UTC).toEpochMilli();
+        // return lessThan(ldt1, ldt2) //
+        //         || ldt1.toInstant(ZoneOffset.UTC).toEpochMilli() == ldt2.toInstant(ZoneOffset.UTC).toEpochMilli();
+        return ldt1.isBefore(ldt2) || ldt1.isEqual(ldt2);
     }
 
     /** @return true if the {@link LocalDateTime} @param ldt1 is strictly less than
      *         the {@link LocalDateTime} @param ldt2 and false otherwise. */
+    @Deprecated // redundant, see below
     public static boolean lessThan(LocalDateTime ldt1, LocalDateTime ldt2) {
-        // TODO use ldt1.compareTo(ldt2) == XY?
-        return ldt1.toInstant(ZoneOffset.UTC).toEpochMilli() < //
-                ldt2.toInstant(ZoneOffset.UTC).toEpochMilli();
+        // return ldt1.toInstant(ZoneOffset.UTC).toEpochMilli() < //
+        //         ldt2.toInstant(ZoneOffset.UTC).toEpochMilli();
+        return ldt1.isBefore(ldt2);
     }
 
     /** @return the {@link LocalDateTime} @param localDateTime to which @param duration were added, works
