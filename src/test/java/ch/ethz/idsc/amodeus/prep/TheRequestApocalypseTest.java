db6e55a0b3ff0204e94e3deb1464099ef203ab66
JoelGaechter <joelgaechter@bluewin.ch>
2020-05-15 16:55:03 +0200

    TheRequestApocalypseTest

--- src/test/java/ch/ethz/idsc/amodeus/prep/TheRequestApocalypseTest.java
+++ src/test/java/ch/ethz/idsc/amodeus/prep/TheRequestApocalypseTest.java
@@ -2,47 +2,45 @@
 package ch.ethz.idsc.amodeus.prep;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
 
-import ch.ethz.idsc.tensor.io.DeleteDirectory;
+import ch.ethz.idsc.amodeus.test.ScenarioExecutionTest;
+import ch.ethz.idsc.amodeus.test.TestFileHandling;
+import ch.ethz.idsc.amodeus.util.io.Locate;
+import ch.ethz.idsc.amodeus.util.io.MultiFileTools;
+import ch.ethz.idsc.amodeus.util.math.GlobalAssert;
 import junit.framework.TestCase;
+import org.matsim.api.core.v01.population.Population;
 
 public class TheRequestApocalypseTest extends TestCase {
 
     public void testSimple() throws Exception {
-        // TODO @joel reinstate test
-        // /** download a scenario */
-        // StaticHelper.setupScenario();
-        //
-        // /** prepare a scenario */
-        // Preparer preparer = new Preparer();
-        // Population population = preparer.population;
-        //
-        // PopulationCutter populationCutter = preparer.scenOpt.getPopulationCutter();
-        // populationCutter.cut(population, preparer.network, preparer.config);
-        //
-        // /** reduce the number of legs */
-        // int numReqDes = 5000;
-        // long seed = 1234;
-        // TheRequestApocalypse.reducesThe(population).toNoMoreThan(RationalScalar.of(numReqDes, 1), seed);
-        //
-        // /** ensure testing worked correctly */
-        // assertEquals(numReqDes, LegCount.of(population, "av").number().intValue());
+        File workingDirectory = MultiFileTools.getDefaultWorkingDirectory();
 
-        /** clean scenario */
-        // TODO @clruch clean up the scenario
+        /** download a scenario */
+        File scenarioDirectory = //
+                new File(Locate.repoFolder(ScenarioExecutionTest.class, "amodeus"), "resources/testScenario");
+        GlobalAssert.that(workingDirectory.isDirectory());
+        TestFileHandling.copyScnearioToMainDirectory(scenarioDirectory.getAbsolutePath(), //
+                workingDirectory.getAbsolutePath());
 
+        /** prepare a scenario */
+        Preparer preparer = new Preparer(workingDirectory);
+        Population population = preparer.population;
+
+        PopulationCutter populationCutter = preparer.scenOpt.getPopulationCutter();
+        populationCutter.cut(population, preparer.network, preparer.config);
+
+        /** reduce the number of legs */
+        int numReqDes = 3000;
+        long seed = 1234;
+        TheRequestApocalypse.reducesThe(population).toNoMoreThan(numReqDes, seed);
+
+        /** ensure testing worked correctly */
+        assertEquals(numReqDes, (int) LegCount.of(population, "av"));
     }
 
     @Override
     public void tearDown() throws Exception {
-        List<File> list = Arrays.asList(//
-                new File("preparedNetwork.xml.gz"), //
-                new File("network.xml.gz"), //
-                new File("population.xml.gz"));
-        for (File file : list)
-            if (file.exists())
-                DeleteDirectory.of(file, 1, 1);
+        TestFileHandling.removeGeneratedFiles();
     }
 }
