a0e5a7630a34a52980d1f0433aba51017cc0e690
JoelGaechter <joelgaechter@bluewin.ch>
2020-06-02 15:08:45 +0200

    adjust Locate

--- src/main/java/amodeus/amodeus/util/io/Locate.java
+++ src/main/java/amodeus/amodeus/util/io/Locate.java
@@ -11,14 +11,18 @@ public enum Locate {
     /** @return top folder of repository with name @param repoName in which
      *         some {@link Class} @param someClass is located. Usage sample:
      * 
-     *         File directory = new File(LocateUtils.getSuperFolder(CurrentClass.class,"amodeus"), "resources/testScenario"); */
+     *         File directory = new File(LocateUtils.getSuperFolder(CurrentClass.class, "amodeus"), "resources/testScenario"); */
     public static File repoFolder(Class<?> someClass, String repoName) {
         File file = new File(someClass.getResource(someClass.getSimpleName() + ".class").getFile());
         GlobalAssert.that(file.getAbsolutePath().contains(repoName));
         boolean test = true;
-        while (!file.getName().endsWith(repoName) || test) {
+        boolean amodeus = repoName.equals("amodeus");
+        while (!file.getName().endsWith(repoName) || test || amodeus) {
             if (file.getName().endsWith(repoName))
-                test = false;
+                if (amodeus)
+                    amodeus = false;
+                else
+                    test = false;
             file = file.getParentFile();
         }
         return file;
